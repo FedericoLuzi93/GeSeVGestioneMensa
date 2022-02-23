a001dd8d049daa7993a81c3c364a9337d3e411a3
Federico <federico.luzi93@gmail.com>
2022-02-23 12:36:14 +0100

    Inserimento Classi
    Inserimento EsitoDTO e GeSeVException

---
+++ src/main/java/it/gesev/mensa/exc/GesevException.java
@@ -0,0 +1,29 @@
+package it.gesev.mensa.exc;
+
+import org.springframework.http.HttpStatus;
+
+public class GesevException extends RuntimeException
+{
+
+	private static final long serialVersionUID = 7854865386639091308L;
+	private HttpStatus status;
+	
+	public GesevException(String message)
+	{
+		super(message);
+	}
+	
+	public GesevException(String message, HttpStatus status)
+	{
+		super(message);
+		this.status = status;
+	}
+
+	public HttpStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+}
